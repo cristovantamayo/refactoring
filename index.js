44c0590b32b87a209e9ef2aedbbbe575a5fbec76
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2026-06-09 19:29:25 -0300

    Change Function Declaration

--- index.js
+++ index.js
@@ -26,23 +26,30 @@ function amountFor(aPerformance){
     return result
 }
 
+function volumeCreditsFor(perf){
+    let result = 0; 
+    result += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
+    return result;
+}
+
+function usd(aNumber){
+    return new Intl.NumberFormat("en-US", 
+        { style: "currency", currency: "USD", 
+            minimumFractionDigits: 2 }).format(aNumber);
+}
+
 function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
-
     for (let perf of invoice.performances) { 
-        const thisAmount = amountFor(perf);
-        // soma créditos por volume 
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // soma um crédito extra para cada dez espectadores de comédia 
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits = volumeCreditsFor(perf);
         // exibe a linha para esta requisição 
-        result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+        totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
